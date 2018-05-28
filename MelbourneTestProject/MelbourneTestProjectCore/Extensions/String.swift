05025fed6234486aecbee8d2591a8be325b531b7
Yevhen Herasymenko <yhe@ciklum.com>
2018-05-28 21:16:53 +0300

    [MTP-2] add / method to String

---
+++ MelbourneTestProject/MelbourneTestProjectCore/Extensions/String.swift
@@ -0,0 +1,17 @@
+//
+//  String.swift
+//  MelbourneTestProjectCore
+//
+//  Created by Yevhen Herasymenko on 5/28/18.
+//  Copyright © 2018 Yevhen Herasymenko. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    static func / (left: String, right: String) -> String {
+        return left + "/" + right
+    }
+    
+}

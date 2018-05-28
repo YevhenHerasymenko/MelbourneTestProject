106def3e983d30b829a2d3e7cb735ddc9a112e18
Yevhen Herasymenko <yhe@ciklum.com>
2018-05-28 22:16:56 +0300

    [MTP-4] add models for facts request

---
+++ MelbourneTestProject/MelbourneTestProjectCore/New Group/Fact.swift
@@ -0,0 +1,9 @@
+//
+//  Fact.swift
+//  MelbourneTestProjectCore
+//
+//  Created by Yevhen Herasymenko on 5/28/18.
+//  Copyright © 2018 Yevhen Herasymenko. All rights reserved.
+//
+
+import Foundation
---
+++ MelbourneTestProject/MelbourneTestProjectCore/New Group/FactGroup.swift
@@ -0,0 +1,9 @@
+//
+//  FactGroup.swift
+//  MelbourneTestProjectCore
+//
+//  Created by Yevhen Herasymenko on 5/28/18.
+//  Copyright © 2018 Yevhen Herasymenko. All rights reserved.
+//
+
+import Foundation
